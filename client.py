0dd448a4f6682d1b8209558d2bbbd5ccbd81421e
ahmedhasni <ahmed.hasni@yoterra.com>
2022-04-28 09:38:40 +0100

    commit 5

--- client.py
+++ client.py
@@ -1,36 +1,160 @@
 import socket
+import tkinter
+import tkinter.messagebox
+import customtkinter
+import sys
 
-def main_authentification():
-    print("Donner la reference de votre compte")
-    ref = input(">> ") # utilisez raw_input() pour les anciennes versions python
-    s.send(b"auth"+ref.encode())
-    r = s.recv(9999999)
-    print(r)
-    return(r,ref)
+customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
+customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
-def main_client(ref):
+
+class App(customtkinter.CTk):
+
+    WIDTH = 780
+    HEIGHT = 520
+
+    def __init__(self):
+        super().__init__()
+        self.title("Soukra Bank")
+        self.geometry(f"{App.WIDTH}x{App.HEIGHT}")
+        # self.minsize(App.WIDTH, App.HEIGHT)
+
+        self.protocol("WM_DELETE_WINDOW", self.on_closing)  # call .on_closing() when app gets closed
+
+        # ============ create two frames ============
+
+        # configure grid layout (2x1)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_rowconfigure(0, weight=1)
+
+        self.frame_left = customtkinter.CTkFrame(master=self,
+                                                 width=180,
+                                                 corner_radius=0)
+        self.frame_left.grid(row=0, column=0, sticky="nswe")
+
+        self.frame_right = customtkinter.CTkFrame(master=self)
+        self.frame_right.grid(row=0, column=1, sticky="nswe", padx=20, pady=20)
+
+        # ============ frame_left ============
+
+        # configure grid layout (1x11)
+        self.frame_left.grid_rowconfigure(0, minsize=10)   # empty row with minsize as spacing
+        self.frame_left.grid_rowconfigure(5, weight=1)  # empty row as spacing
+        self.frame_left.grid_rowconfigure(8, minsize=20)    # empty row with minsize as spacing
+        self.frame_left.grid_rowconfigure(11, minsize=10)  # empty row with minsize as spacing
+
+        self.label_1 = customtkinter.CTkLabel(master=self.frame_left,
+                                              text="Bank of Soukra",
+                                              text_font=("Roboto Medium", -16))  # font name and size in px
+        self.label_1.grid(row=1, column=0, pady=10, padx=10)
+        self.button_1 = customtkinter.CTkButton(master=self.frame_left,
+                                                text="Debit",
+                                                fg_color=("gray75", "gray30"),  # <- custom tuple-color
+                                                command=self.button_event)
+        self.button_1.grid(row=2, column=0, pady=10, padx=20)
+
+        self.button_2 = customtkinter.CTkButton(master=self.frame_left,
+                                                text="Credit",
+                                                fg_color=("gray75", "gray30"),  # <- custom tuple-color
+                                                command=self.button_event)
+        self.button_2.grid(row=3, column=0, pady=10, padx=20)
+
+        self.button_3 = customtkinter.CTkButton(master=self.frame_left,
+                                                text="withdraw an invoice",
+                                                fg_color=("gray75", "gray30"),  # <- custom tuple-color
+                                                command=self.button_event)
+        self.button_3.grid(row=4, column=0, pady=10, padx=20)
+
+        self.switch_2 = customtkinter.CTkSwitch(master=self.frame_left,
+                                                text="Dark Mode",
+                                                command=self.change_mode)
+        self.switch_2.grid(row=10, column=0, pady=10, padx=20, sticky="w")
+
+
+        # ============ frame_right ============
+
+        # ============ frame_info ============
+
+        # configure grid layout (1x1)
+
+
+        # ============ frame_right ============
+
+        self.entry = customtkinter.CTkEntry(master=self.frame_right,
+                                            width=120,
+                                            placeholder_text="Enter your Code")
+        self.entry.grid(row=1, column=0, columnspan=2, pady=20, padx=20, sticky="we")
+        self.entry = customtkinter.CTkEntry(master=self.frame_right,
+                                            width=120,
+                                            placeholder_text="Type the amount")
+        self.entry.grid(row=2, column=0, columnspan=2, pady=20, padx=20, sticky="we")
+
+        self.button_1 = customtkinter.CTkButton(master=self.frame_right,
+                                                text="Debit",
+                                                fg_color=("gray75", "gray30"),  # <- custom tuple-color
+                                                command=self.button_event)
+        self.button_1.grid(row=3, column=1, columnspan=1, pady=20, padx=20, sticky="we")
+
+        self.button_2 = customtkinter.CTkButton(master=self.frame_right,
+                                                text="Credit",
+                                                fg_color=("gray75", "gray30"),  # <- custom tuple-color
+                                                command=self.button_event)
+        self.button_2.grid(row=3, column=2, columnspan=1, pady=20, padx=20, sticky="we")
+        self.button_3 = customtkinter.CTkButton(master=self.frame_right,
+                                                text="withdraw an invoice",
+                                                fg_color=("gray75", "gray30"),  # <- custom tuple-color
+                                                command=self.button_event)
+        self.button_3.grid(row=3, column=3, columnspan=1, pady=20, padx=20, sticky="we")
+
+        # set default values
+        self.switch_2.select()
+
+        #self.slider_button_1.configure(state=tkinter.DISABLED, text="Disabled Button")
+        #self.radio_button_3.configure(state=tkinter.DISABLED)
+        #self.check_box_1.configure(state=tkinter.DISABLED, text="CheckBox disabled")
+        #self.check_box_2.select()
+
+    def button_event(self):
+        print("Button pressed")
+
+    def change_mode(self):
+        if self.switch_2.get() == 1:
+            customtkinter.set_appearance_mode("dark")
+        else:
+            customtkinter.set_appearance_mode("light")
+
+    def on_closing(self, event=0):
+        self.destroy()
+
+    def start(self):
+        self.mainloop()
+
+
+
+def main_client():
     print("choisissez l'operation a executer")
     print("1. operation de depot")
     print("2. operation de retrait")
     choix=input("valeur de choix")
     if (choix=="1"):
         montant=input("saisir le montant a deposer")
-        return("depot,"+ref+","+montant)
+        return(montant)
     
     elif(choix=="2"):
         montant=input("saisir le montant a retirer")
-        return("retrait,"+ref+","+montant)
+        return(montant)
     else: return(-1)
+if __name__ == "__main__":
+    app = App()
+    app
+    while True:
+     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+     s.connect(("192.168.1.1", 50000))
+     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+     s.connect(("192.168.1.1", 50000))
+     request=main_client()
+     s.send(request.encode())
+     result = s.recv(1024)
+     print(result)
 
-while True:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect(("192.168.1.9", 50000))
-    rm=main_authentification()
-    print(rm)
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect(("192.168.1.9", 50000))
-    request=main_client(rm[1])
-    s.send(request.encode())
-    result = s.recv(9999999)
-    print(result)
-   
+
--- server.py
+++ server.py
@@ -7,96 +7,11 @@ import select,time,threading
 import os
 import csv
   
-auction = False
-reference= "test"
-bidder =""
-bidtime= datetime.now()
-prix = 0
-global prixinit
-joins =[]
-done = False
-factures={}
-#processus time 
-class TestThreading(object):
-    global bidtime
-    global bidder
-    global auction
-    global joins
-    global prix
-    global prixinit
- 
-    def __init__(self, interval=1):
-        self.interval = interval
- 
-        thread = threading.Thread(target=self.run, args=())
-        thread.daemon = True
-        thread.start()
- 
-    def run(self):
-        global bidtime
-        global bidder
-        global auction
-        global done
-        global joins
-        while True:
-            # More statements comes here
- 
- 
- 
-            if ((datetime.now()-bidtime).seconds==25 and not done):
-                diffuserj(bytes("La vente au enchère va cloturer dans 10 seconds" , "utf8"))
-                done = True
-            if ((datetime.now()-bidtime).seconds>35): #auction ended
-                bien = open("bien.txt","a")
-                if (bidder!=""): #bidder exist
-                    diffuserj(bytes("The auction has ended ,%s won." % bidder, "utf8"))
-                    print("The auction has ended ,%s won." % bidder)
-                    bien.write(strbien(reference,prixinit,prix,"Vendu",bidder+"\n"))
-                    bien.close()
-                    histo = open("histo.txt","a")
-                    histo.write(strhisto(bidder,prix,"succes\n"))
-                    histo.close()
-                    #ajouter a factures
-                    if (bidder not in list(factures.keys())):
-                       fact = open("factures.txt","a")
-                       fact.write(strfact(bidder,prix)+"\n")
-                       fact.close()
-                       factures[bidder]=prix
-                    else : #bidder exist deja dans factures
-                        factures[bidder]=factures[bidder]+prix
-                        remplir()
- 
- 
-                    auction = False
-                    joins=[]
-                    bidder=""
-                    break
- 
-                else :  #no bidder . produit disponible
-                    if auction:
-                        diffuserj(bytes("Vente aux enchere a fini sans gagnant", "utf8"))
-                        print("Vente aux enchere a fini sans gagnant")
-                        bien.write(strbien(reference,prixinit,prixinit,"Disponible","N/A\n"))
-                        bien.close()
-                        auction = False
-                        joins=[]
-                        break
- 
- 
-                auction = False
-                joins=[]
-                break
- 
-            time.sleep(self.interval)
 #proccessus menu
 class menu(object):
-    global bidtime
-    global bidder
-    global auction
-    global reference
-    global prix
-    global prixinit
-    global done
+    
+    
+    
  
     def __init__(self, interval=1):
         self.interval = interval
@@ -106,16 +21,9 @@ class menu(object):
         thread.start()
  
     def run(self):
-        global bidtime
-        global bidder
-        global auction
-        global done
-        global prix
-        global prixinit
-        global reference
         while True:
             # More statements comes here
-            if not auction :
+            
                 print(" ***** Soukra Bank ***** ")
                 print("1- Account details ")
                 print("2- Account bill ")
@@ -167,216 +75,69 @@ class menu(object):
  
  
  
-            time.sleep(self.interval)
+        time.sleep(self.interval)
  
  
 def accepter_connexions():
     while True:
         try:
             client, client_address = SERVER.accept()
-            client.send(bytes("Ecrivez votre nom !","utf8"))
-            addresses[client] = client_address
+            client.send(bytes("Connecting...","utf8"))
             Thread(target=gerer_client, args=(client,)).start()
         except:
-            print("")
+            print("Not connected")
             break
  
  
  
  
 def gerer_client(client):  
-    nom = client.recv(BUFSIZ).decode("utf8")
-    global prix
-    global bidder
-    global bidtime
-    global auction
-    global done
-    if 1 :
- 
-        client.send(bytes('Bienvenue %s! ' % nom, "utf8"))
-        tr = TestThreading()
-        if (auction):
-            client.send(bytes('There is an auction going on :'+reference+"("+str(prix)+")\n", "utf8"))
-            client.send(bytes("\n To join the auction use /join ", "utf8"))
-        else :
-            client.send(bytes("There is no auction going on , please wait for a new auction", "utf8"))
- 
-        while True:
- 
-            clients[client] = nom
-            msg = client.recv(BUFSIZ)
-            if auction:
-                if msg == bytes("/join", "utf8") and not joined(nom):
-                    add(nom)
-                    ms = "%s has joined the auction" % nom
-                    diffuserj(bytes(ms, "utf8"))
-                    client.send(bytes(''+reference+", current price ("+str(prix)+") ,\n", "utf8"))
-                    client.send(bytes("To quit the auction use /q ", "utf8"))
-                    print(nom+" has joined the auction"); 
- 
-                else :
-                    if msg == bytes("/join", "utf8") and joined(nom):
-                        client.send(bytes("You are already in the auction! ", "utf8"))
-                if joined(nom):
-                    try :
-                        msgint=int(msg)
-                        #print(nom,"placed a new bid:"+msg)
-                        if (msgint > prix): #prix valide
-                            diffuserj(msg,nom+" made a new bid,the current price is :")
- 
-                            if (bidder!=""):
-                                histo = open("histo.txt","a")
-                                histo.write(strhisto(bidder,prix,"echec\n"))
- 
-                                histo.close()
- 
-                            prix = msgint
-                            bidder = nom
-                            bidtime= datetime.now()
-                            done = False
-                            print("latest bidder : "+nom+"("+str(msgint)+"),"+bidtime.strftime("%H:%M:%S"))
-                        else :
-                            if msg != bytes("/q", "utf8"):
-                                client.send(bytes("wrong bid!", "utf8"))
- 
-                    except:
-                        if (msg != bytes("/q", "utf8") and msg != bytes("/join", "utf8")):
-                            client.send(bytes("wrong input !", "utf8"))
- 
-                if msg == bytes("/q", "utf8"):
-                    if not joined(nom):
-                        client.send(bytes("you are not in an auction !", "utf8"))
-                    if bidder == nom and joined(nom):
-                        client.send(bytes("you can't quit , you are the latest bidder !", "utf8"))
-                    if bidder != nom and joined(nom):
-                        diffuserj(bytes("%s has left the auction." % nom, "utf8"))
-                        print(nom+" has left the auction")
-                        client.send(bytes("To join again use /join", "utf8"))
-                        quit(nom)
-                if not joined(nom) and msg != bytes("/join", "utf8") and msg != bytes("/q", "utf8"):
-                    client.send(bytes("you are not in an action !", "utf8"))
-            else :
-                 client.send(bytes("There is no auction going on , please wait for a new auction", "utf8"))
+    global ref
+    global amount
+    choix = client.recv(BUFSIZ).decode("utf8")
+    #debiter
+    if int(choix)==1 :
+        print("1.....")
+        client.send(bytes('donner votre reference',"utf8"))
+        ref= client.recv(BUFSIZ).decode("utf8")
+        print("2.....")
+        client.send(bytes('donner le montant a debiter',"utf8"))
+        amount= client.recv(BUFSIZ).decode("utf8")
+        print("3.....")
+        with open('comptes.txt', mode='r') as csv_file:
+                        csv_reader = csv.DictReader(csv_file)
+                        exist=False
+                        for row in csv_reader:
+                            if row["Reference"] == ref :
+                                exist=True
+                                if row["Etat"]=="Positif":
+                                    if int(row["valeur"])+int(row["Plafond_Debit"])>int(amount):                                        
+                                        with open('histo.csv', mode='w') as histo_file:
+                                         histo_writer = csv.writer(histo_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+                                         histo_writer.writerow([ref,'Retrait',amount, 'succes','Positif'])
+                                        with open('comptes.csv', mode='w') as compte_file:
+                                         compte_file=csv.writer(compte_file,delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)           
+                                         compte_file.writerow([ref,int(row["valeur"])-int(amount)],'Positif',row["Plafond_Debit"])
+                                         print("jawha behi")
+                                    else :
+                                         client.send(bytes('No so broke',"utf8"))
+                                else:
+                                    if int(row["Plafond_Debit"])-int(row["valeur"])>int(amount):            
+                                        with open('histo.csv', mode='w') as histo_file:
+                                         histo_writer = csv.writer(histo_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+                                         histo_writer.writerow([ref,'Retrait',amount, 'succes','Negatif'])
+                                        with open('comptes.csv', mode='w') as compte_file:
+                                         compte_file=csv.writer(compte_file,delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)           
+                                         compte_file.writerow([ref,int(row["valeur"])-int(amount)],'Negatif',row["Plafond_Debit"])
+                        if (exist==False): client.send(bytes('Compte non existant',"utf8"))
+
+                          
  
  
  
- 
- 
- 
-#diffuser un message a tous les client connectés
-def diffuser(msg, prefix=""): 
- 
-    for sock in clients:
-        sock.send(bytes(prefix, "utf8")+msg)
-#diffuser un message au clients joins
-def diffuserj(msg, prefix=""): 
- 
-    for sock in clients:
-        if joined(clients[sock]):
-            sock.send(bytes(prefix, "utf8")+msg)
  
  
 
-#fonction qui ajout un client a la liste des client joints
-def add(nom):
-    global joins
-    joins.append(nom)
- 
-#l'inverse de add
-def quit(nom):
-    global joins
-    joins.remove(nom)
- 
-#fonction pour lire les factures du fichier factures.txt et remplir le
-#dictionnaire factures.
-def remplirfactures():
-    global factures
-    fact = open("factures.txt","a")
-    fact.close()
-    fact = open("factures.txt","r")
-    l = fact.readlines()
-    fact.close()
-    for i in l:
-        nom = i.split(" ")[0]
-        p = i.split(" ")[-1]
-        if p[-1]=="\n":
-            p = p[:-1]
-        factures[nom]='int(p)'
- 
-#fonction pour remplir le fichier factures apres mise a jour 
-def remplir():
-    global factures
-    fact = open("factures.txt","a")
-    fact.close()
-    fact = open("factures.txt","w")
-    for i in factures:
-        fact.write(strfact(i,factures[i])+"\n")
-    fact.close()
- 
-def strfact(a,b):
-    ch = a
-    for i in range (0,27-len(a)):
-        ch=ch+" "
-    ch = ch +str(b)
-    return ch
- 
- 
-def strbien(ref,p1,p2,t,bid):
-    ch = ref
-    for i in range (0,17-len(ref)):
-        ch = ch+" "
-    ch = ch +"|"
-    for i in range (0,4):
-        ch = ch+" "
- 
- 
- 
-    ch = ch+str(p1)
-    for i in range (0,12-len(str(p1))):
-        ch = ch+" "
-    ch = ch +"|"
-    for i in range (0,4):
-        ch = ch+" "
-    ch = ch+str(p2)
-    for i in range (0,15-len(str(p2))):
-        ch = ch+" "
-    ch = ch +"|"
-    for i in range (0,4):
-        ch = ch+" "
-    ch = ch+t
-    for i in range (0,15-len(t)):
-        ch = ch+" "
-    ch = ch +"|"
-    for i in range (0,4):
-        ch = ch+" "
-    ch = ch+bid
-    return ch
- 
-def strhisto(bid,p,t):
-    ch = bidder
-    for i in range (0,20-len(bid)):
-        ch=ch+" "
-    ch = ch +"|"
-    for i in range (0,4):
-        ch = ch+" "
-    ch = ch + str(p)
-    for i in range (0,12-len(str(p))):
-        ch = ch+" "
-    ch = ch +"|"
-    for i in range (0,4):
-        ch = ch+" "
-    ch = ch+t
-    return ch
- 
- 
- 
- 
- 
- 
- 
-clients = {}
-addresses = {}
- 
  
 
 HOST = '192.168.1.1'
@@ -390,7 +151,6 @@ SERVER.bind(ADDR)
 if __name__ == "__main__":
     while True :
  
-        remplirfactures()
         now = datetime.now()
         current_time = now.strftime("%H:%M:%S")
  
