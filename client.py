111745b86b786d7249c54e3e99820cf0f6eee220
BahaGodx <baha.thabet@live.ca>
2022-04-28 04:14:04 +0100

    aa

--- client.py
+++ client.py
@@ -1,29 +1,84 @@
-import socket
-
-HEADER=64
-port=5050
-SERVER='172.18.3.218'
-
-print(SERVER)
-FORMAT='utf-8'
-DISCONNEST_MSG='!DISCONNECT'
-ADDR=(SERVER , port)
-
-
-client=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-client.connect(ADDR)
-
-
-def send(msg):
-    message = msg.encode(FORMAT)
-    msg_length = len(message)
-    send_length =str(msg_length).encode(FORMAT)
-    send_length += b' ' * (HEADER - len(send_length))
-    client.send (send_length)
-    client.send(message)
-    print (client.recv(2048).decode(FORMAT))
-
-send("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa khdem")    
-input()
-send("Helloo")    
-input()
+from socket import AF_INET, socket, SOCK_STREAM
+from threading import Thread
+import tkinter
+ 
+test = True
+ 
+def receive():
+    while True:
+        try:
+            msg = client_socket.recv(BUFSIZ).decode("utf8")
+            msg_list.insert(tkinter.END, msg)
+        except OSError:
+            msg_list.insert(tkinter.END,"The server just crashed")
+ 
+            break
+ 
+ 
+def send():  
+    msg = my_msg.get()
+    my_msg.set("")  
+    client_socket.send(bytes(msg, "utf8"))
+ 
+def exitc(event=None):
+    #client_socket.close()
+    scene.quit()
+ 
+ 
+ 
+def close_window ():
+ 
+    scene.destroy()
+ 
+ 
+# definition des elements de notre  interface 
+scene = tkinter.Tk()
+scene.title("Vente aux enchere - Application")
+ 
+messages_frame = tkinter.Frame(scene)
+my_msg = tkinter.StringVar()  # For the messages to be sent.
+my_msg.set("name")
+scrollbar = tkinter.Scrollbar(messages_frame)  # To navigate through past messages.
+# Following will contain the messages.
+msg_list = tkinter.Listbox(messages_frame, height=30, width=100, yscrollcommand=scrollbar.set)
+ 
+scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+ 
+ 
+msg_list.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
+msg_list.pack()
+messages_frame.pack()
+ 
+entry_field = tkinter.Entry(scene, textvariable=my_msg)
+entry_field.bind("<Return>", send) #Return=la clé entrer du clavier #elle déclenche la méthode send
+entry_field.pack()
+send_button = tkinter.Button(scene, text="Envoyer", command=send)
+send_button.pack()
+button = tkinter.Button (scene, text = "Quitter", command = close_window)
+button.pack()
+ 
+ 
+ 
+ 
+HOST = '192.168.1.9'
+if not HOST:
+    HOST= '192.168.1.9'
+PORT = 50000
+if not PORT:
+    PORT = 33000
+else:
+    PORT = int(PORT)
+ 
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+try:
+    client_socket = socket(AF_INET, SOCK_STREAM) #le type du socket : SOCK_STREAM pour le protocole TCP
+    client_socket.connect(ADDR)
+    test = True
+except :
+    print("Serveur est déconnecté")
+    test = False
+if test :
+    receive_thread = Thread(target=receive)
+    receive_thread.start()
+    tkinter.mainloop()
